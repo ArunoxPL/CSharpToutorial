cf279631d5cbfa96dae6d84caf176d884946d86d
ArunoxPL <greg.malek@gmail.com>
2017-04-02 14:40:02 +0200

    C# first commit

---
+++ Toutorial/Toutorial2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toutorial2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Witaj świecie !!!");
+        }
+    }
+}
