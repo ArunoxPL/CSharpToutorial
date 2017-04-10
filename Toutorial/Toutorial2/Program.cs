2cb763c38278e495f267e701ba99277a83adf660
ArunoxPL <greg.malek@gmail.com>
2017-04-10 22:36:37 +0200

    C# begining

--- Toutorial/Toutorial2/Program.cs
+++ Toutorial/Toutorial2/Program.cs
@@ -8,6 +8,7 @@ namespace Toutorial2
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
             var Number = 10;
@@ -26,6 +27,12 @@ namespace Toutorial2
             */
             IsNumber10 = Number == 10 ? true : false;
             Console.WriteLine("Number == 10 is {0}", IsNumber10);
+            int[] liczby = { 1, 2, 3, 4};
+            foreach(int i in liczby)
+            {
+                Console.WriteLine(i);
+            }
+
 
             //System.Console.WriteLine("Witaj świecie !!!");
 
